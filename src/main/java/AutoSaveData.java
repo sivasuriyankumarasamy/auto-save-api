2d5f534d74d5da4b0e194060e122bccbf34497fa
sivasuriyankumarasamy <sivasuriyankumarasamy@gmail.com>
2023-10-02 01:42:48 +0530

    added bank nifty api

--- src/main/java/AutoSaveData.java
+++ src/main/java/AutoSaveData.java
@@ -20,7 +20,11 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
+
 public class AutoSaveData {
+
+    private static List<String> API_SYMBOL_LIST = asList("NIFTY", "BANKNIFTY");
 
     public static void main(String[] args) {
         WebClient client = WebClient.builder()
@@ -31,35 +35,52 @@ public class AutoSaveData {
                                 .maxInMemorySize(16 * 1024 * 1024))
                         .build())
                 .build();
+
         final Timer timer = new Timer();
-        XSSFWorkbook workbook = new XSSFWorkbook();
         timer.schedule(new TimerTask() {
                            public void run() {
-                               Optional<StockData> stockDataOptional = getStockData(client);
-                               stockDataOptional.ifPresent(
-                                       stockData -> {
-                                           try {
-                                               generateExcel(workbook, stockData);
-                                           } catch (IOException e) {
-                                               System.out.println("Could not save data");
-                                           }
-                                       }
-                               );
+                               LocalDateTime localDateTime = LocalDateTime.now();
+                               if (isTimePassed(localDateTime)) {
+                                   System.out.println("Stopping the application");
+                                   timer.cancel();
+                               }
+
+                               try {
+                                   process(client);
+                               } catch (IOException e) {
+                                   System.out.println("Exception occurred: " + e.getMessage());
+                               }
+
                            }
                        }, 15 * 1000, 15 * 1000
         );
-
     }
 
-    public static void generateExcel(XSSFWorkbook wb, StockData stockData) throws IOException {
+    private static void process(WebClient client) throws IOException {
         XSSFWorkbook workbook = new XSSFWorkbook();
         LocalDateTime localDateTime = LocalDateTime.now();
-        int hour = localDateTime.getHour();
-        int minute = localDateTime.getMinute();
-        int sec = localDateTime.getSecond();
 
+        for (String api : API_SYMBOL_LIST) {
+            Optional<StockData> stockDataOptional = getStockData(client, api);
+            if (stockDataOptional.isPresent()) {
+                workbook = generateWorkBook(stockDataOptional.get(), api, workbook);
+            }
+        }
+
+        System.out.println("Writing data as Excel file");
+        FileOutputStream out = new FileOutputStream(
+                "./nseindia/data_" + localDateTime.toLocalDate() + "(" + localDateTime.getHour() + "-" + localDateTime.getMinute() + "-" + localDateTime.getSecond() + ").xlsx");
+        workbook.write(out);
+        out.close();
+    }
+
+    private static boolean isTimePassed(LocalDateTime localDateTime) {
+        return localDateTime.getHour() >= 15 && localDateTime.getMinute() >= 30;
+    }
+
+    public static XSSFWorkbook generateWorkBook(StockData stockData, String sheetName, XSSFWorkbook workbook) {
         XSSFSheet spreadsheet
-                = workbook.createSheet(" Stock data ");
+                = workbook.createSheet(sheetName);
 
         // creating a row object
         int rowid = 0;
@@ -67,16 +88,14 @@ public class AutoSaveData {
 
 
         // writing the data into the sheets...
-
         List<MetaData> stockMetaData = stockData.getStocks().stream()
-//                .filter(stocks -> "28-Sep-2023".equals(stocks.getMetadata().getExpiryDate()))
                 .map(stocks -> stocks.getMetadata())
                 .collect(Collectors.toList());
 
         Object[] headerArr = new Object[]{
-                "INSTRUMENT TYPE", "EXPIRY DATE", "OPTION", "STRIKE", "OPEN", "HIGH", "LOW", "CLOSE", "PREV.CLOSE", "LAST", "CHNG", "%CHNG","VOLUME(Contracts)", "VALUE(₹ Lakhs)"
+                "INSTRUMENT TYPE", "EXPIRY DATE", "OPTION", "STRIKE", "OPEN", "HIGH", "LOW", "CLOSE",
+                "PREV.CLOSE", "LAST", "CHNG", "%CHNG", "VOLUME(Contracts)", "VALUE(₹ Lakhs)"
         };
-//        row = spreadsheet.createRow(rowid++);
         insertCellData(row, headerArr);
 
         for (MetaData stock : stockMetaData) {
@@ -86,16 +105,7 @@ public class AutoSaveData {
                     stock.getPrevClose(), stock.getLastPrice(), stock.getChange(), stock.getpChange(), stock.getNumberOfContractsTraded(), stock.getTotalTurnover()};
             insertCellData(row, objectArr);
         }
-
-        // .xlsx is the format for Excel Sheets...
-        // writing the workbook into the file...
-        FileOutputStream out = new FileOutputStream(
-//                "D:/datasheet.xslx");
-                "D:/nseindia/data_" + localDateTime.toLocalDate() + "(" + hour + "-" + minute + "-" + sec + ").xlsx");
-//        FileOutputStream out = new FileOutputStream(
-//                new File("C:/savedexcel/GFGsheet.xlsx"));
-        workbook.write(out);
-        out.close();
+        return workbook;
     }
 
     private static void insertCellData(XSSFRow row, Object[] objectArr) {
@@ -107,18 +117,16 @@ public class AutoSaveData {
         }
     }
 
-    public static Optional<StockData> getStockData(WebClient client) {
+    public static Optional<StockData> getStockData(WebClient client, String api) {
+        System.out.println("Fetching data from API");
         Mono<StockData> result = client.get()
-                .uri("/api/quote-derivative?symbol=NIFTY").accept(MediaType.APPLICATION_JSON)
+                .uri("/api/quote-derivative?symbol=" + api).accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, clientResponse ->
                         Mono.error(new ServerException(" server error")))
                 .bodyToMono(StockData.class)
                 .retryWhen(Retry.backoff(30, Duration.ofSeconds(3)));
 
-        Optional<StockData> stockDataOptional = result.blockOptional();
-
-        return stockDataOptional;
-
+        return result.blockOptional();
     }
 }
